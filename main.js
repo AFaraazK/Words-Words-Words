cf59949a8e8cb4a4cd47bb80a0f140f9f5ac5f64
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-10-08 21:59:44 -0400

    Randomize Letter Generation

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 const typing = document.querySelector(".typing");
-const resetButton = document.querySelector(".reset");
+const clearButton = document.querySelector(".clear");
 const enterButton = document.querySelector(".submit")
 const letterButtons = document.querySelectorAll(".letter")
 
-resetButton.addEventListener('click', e => {
+clearButton.addEventListener('click', e => {
     typing.innerHTML = "";
 })
 
 letterButtons.forEach(letter => {
     letter.addEventListener('click', e => {
-        let selected = letter.innerHTML;
-        alert(`Clicked on ${selected}`);
+        let current_display = typing.textContent;
+        let selected = letter.textContent;
+        let updated = typing.textContent + selected; 
+        if(updated.length < 7){
+            typing.textContent = updated;
+        }
     })
 })
 
 enterButton.addEventListener('click', e => {
     alert("Enter");
 })
+
+function submit(){
+
+}
+
+function randomizeLetters(){
+    const result = [];
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    while(result.length < 6){
+        let ran = Math.floor(Math.random() * 26);
+        let ranA = alphabet.charAt(ran)
+        if(!(result.includes(ranA))){
+            result.push(ranA);
+        }
+    }
+
+    // place letters into DOM
+    letterButtons.forEach((letter, index) => {
+        letter.textContent = String(result[index]).toUpperCase();
+    })
+}
+
+randomizeLetters()
